c3c981b009ac15ff7ed3a0911498a62f3c19c75b
Artur Komoter <artur.komoter@growcode.com>
2023-08-09 16:02:24 +0200

    MRSL-128 - Fix displaying historical price message on configurable PDP

--- view/frontend/web/js/historical-price.js
+++ view/frontend/web/js/historical-price.js
@@ -3,12 +3,25 @@ define([
     'Magento_Catalog/js/price-utils'
 ], function ($, utils) {
     return function (config, element) {
-        $(config.priceBox).on('reloadPrice', function () {
+        $(config.priceBox).on('priceUpdated', function (event, data) {
+            if (data.finalPrice.amount === data.oldPrice.amount) {
+                $(element).hide();
+
+                return;
+            }
+
             const selectedConfigurableOption = $('[name="selected_configurable_option"]').val();
 
             if (selectedConfigurableOption) {
-                const price = utils.formatPrice(config.lowestHistoricalPrices[selectedConfigurableOption], config.priceFormat);
-                $(element).find('.price').text(price);
+                const lowestHistoricalPrice = config.lowestHistoricalPrices[selectedConfigurableOption];
+
+                if (lowestHistoricalPrice) {
+                    const price = utils.formatPrice(lowestHistoricalPrice, config.priceFormat);
+                    $(element).find('.price').text(price);
+                    $(element).show();
+                } else {
+                    $(element).hide();
+                }
             }
         });
     }
